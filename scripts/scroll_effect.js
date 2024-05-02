3a365862ae6ba078dbd063ea1d325bad603f6274
Manpreet Singh <128918246+MSingh-CSE@users.noreply.github.com>
2024-05-02 17:04:09 -0300

    fix: hover on mobile

--- scripts/scroll_effect.js
+++ scripts/scroll_effect.js
@@ -29,3 +29,29 @@ const updateNavLinks = (sectionId) => {
     activeNavLink.classList.add('active');
   }
 };
+
+// Add hover only for pointer devices
+function watchForHover() {
+  let lastTouchTime = 0
+
+  function enableHover() {
+    if (new Date() - lastTouchTime < 500) return
+    document.body.classList.add('hasHover')
+  }
+
+  function disableHover() {
+    document.body.classList.remove('hasHover')
+  }
+
+  function updateLastTouchTime() {
+    lastTouchTime = new Date()
+  }
+
+  document.addEventListener('touchstart', updateLastTouchTime, true)
+  document.addEventListener('touchstart', disableHover, true)
+  document.addEventListener('mousemove', enableHover, true)
+
+  enableHover()
+}
+
+watchForHover()
